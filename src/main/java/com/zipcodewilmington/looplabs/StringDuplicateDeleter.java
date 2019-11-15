6aaede40b3cbae99d3e85549800e91753fab1ab2
SandyPham823 <sandydupham@gmail.com>
2019-11-15 11:27:59 -0500

    your friendly neighborhood commit

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,26 @@
 package com.zipcodewilmington.looplabs;
 
+import com.sun.org.apache.xml.internal.utils.res.StringArrayWrapper;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    private static String[] stringArray;
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new String[0];
+    }
 }
